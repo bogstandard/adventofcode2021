135b4af576b1d76b8acb1b3687418089a94fd422
Eric <eric.daddio@gmail.com>
2021-12-06 16:46:58 +0000

    Poor golfed!

--- d-6/one.op.ba.sh
+++ d-6/one.op.ba.sh
@@ -1,49 +1,18 @@
-fishes=" "`<i`" "
-fishes=${fishes//,/ }
-cs=(0 0 0 0 0 0 0 0 0)
-echo "intial: $fishes"
+# expecting 393019
+p=`<i`
+p=${p//,/ }
+x=(0 0 0 0 0 0 0 0 0)
 
-for f in $fishes
+for f in $p
 {
-  cs[$f]=$[cs[$f]+1]
+  x[$f]=$[x[$f]+1]
 }
 
-list(){
-  echo
-  echo "day $day"
-
-  # tabular
-  for c in {0..8}
-  {
-    echo "${cs[$c]} x $c's"
-  }
-
-  echo
-
-  # old school
-  total=0
-  for c in {0..8}
-  {
-    for ((n=0; n<${cs[$c]};n++))
-    {
-      ((total++))
-    }
-  }
-  echo
-
-
-  echo total of $total
-}
-
-list
-
-for day in {1..80}
+for d in {1..80}
 {
-
-  t=("${cs[@]}")
+  t=("${x[@]}")
   for f in {0..7}
   {
-
     # 0s take on 1s & birth new
     # 1s take on 2s
     # 2s take on 3s
@@ -53,11 +22,20 @@ for day in {1..80}
     # 7s take on 8s
     # 8s take on 0s
     n=$[f+1]
-    t[$f]=${cs[$n]}
+    t[$f]=${x[$n]}
   }
-  t[6]=$[cs[0]+cs[7]]
-  t[8]=${cs[0]}
+  t[6]=$[x[0]+x[7]]
+  t[8]=${x[0]}
 
-  cs=("${t[@]}")
-  list
+  x=("${t[@]}")
+
+  T=0
+  for c in {0..8}
+  {
+    for ((n=0; n<${x[$c]};n++))
+    {
+      ((T++))
+    }
+  }
+  echo $T
 }
