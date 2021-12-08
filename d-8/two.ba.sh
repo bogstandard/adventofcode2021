5f35e7f879c252a7896d26b8f7bd04bd4b26811e
Eric <eric.daddio@gmail.com>
2021-12-08 20:11:53 +0000

    Golfed Two! Pip Pip!

--- d-8/two.ba.sh
+++ d-8/two.ba.sh
@@ -1,95 +1,63 @@
 # expecting 1091165
 
 # unique IDs
-# these were worked from the example
-ids=()
-ids[49]=8
-ids[37]=5
-ids[34]=2
-ids[39]=3
-ids[25]=7
-ids[45]=9
-ids[41]=6
-ids[30]=4
-ids[42]=0
-ids[17]=1
+# these were worked from the example because we know how the clock is displayed
+d=()
+d[49]=8
+d[37]=5
+d[34]=2
+d[39]=3
+d[25]=7
+d[45]=9
+d[41]=6
+d[30]=4
+d[42]=0
+d[17]=1
 
-ord() {
+p() {
   LC_CTYPE=C printf '%d' "'$1"
 }
 
-count() {
-  r=`ord "$1"`
-  ((count[$r]++))
-}
-
-get() {
-  r=`ord "$1"`
-  echo ${count[$r]}
-}
-
-
 IFS="
 "
-
-total=0
-for line in `<i`
+t=0
+for l in `<i`
 {
-  count=()
+  k=()
   IFS='|'
-  values=($line)
+  v=($l)
   unset IFS
-  input=${values[0]}
-  output=${values[1]}
 
   # letsa go!
-  for word in $input
+  # derive the segment representation using LHS
+  for w in ${v[0]}
   {
-    for ((i=0; i<${#word}; i++))
+    for ((j=0;j<${#w};j++))
     {
-      char=${word:$i:1}
-      # echo -n counting $char
-      count $char
-      res=$(get $char)
-      # echo -n " ($res)"
-      # echo
+      c=${w:$j:1}
+      ((k[`p "$c"`]++))
     }
-    # echo
   }
 
-  # get unique ids
-  for word in $input
+  # string together RHS
+  c=""
+  for w in ${v[1]}
   {
-    id=0
-    for ((i=0; i<${#word}; i++))
+    n=0
+    for((j=0;j<${#w};j++))
     {
-      char=${word:$i:1}
-      res=$(get $char)
-      id=$[id+res]
+      n=$[n+k[`p "${w:$j:1}"`]]
     }
-    # echo $word at $id
+    c=$c${d[$n]}
   }
 
-  # echo
+  # removing leading zeros & sum
+  c=$((10#$c))
+  t=$[t+c]
 
-  cuml=""
-  for word in $output
-  {
-    id=0
-    for ((i=0; i<${#word}; i++))
-    {
-      char=${word:$i:1}
-      res=$(get $char)
-      id=$[id+res]
-    }
-    cuml=$cuml${ids[$id]}
-  }
-  cuml=$((10#$cuml)) # removing leading zeros
-  total=$((total+cuml))
-  echo "$total (+$cuml)"
-
+  # next line
   IFS="
   "
 }
 
-echo $total
+echo $t
